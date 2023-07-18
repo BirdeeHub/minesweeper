6bc472f2b22cae16924fc72a9798ade71a4700fd
BirdeeHub <robindbor@gmail.com>
2023-07-18 11:05:30 -0700

    Instructions Dark Mode

--- app/src/MySweep/InstructionsWindow.java
+++ app/src/MySweep/InstructionsWindow.java
@@ -2,6 +2,8 @@ package MySweep;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.Icon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -9,26 +11,53 @@ import javax.swing.SwingUtilities;
 import javax.swing.SwingConstants;
 import javax.swing.WindowConstants;
 import javax.swing.border.EmptyBorder;
+import javax.swing.plaf.metal.MetalToggleButtonUI;
 import java.awt.Font;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.KeyboardFocusManager;
+import java.awt.Color;
 import java.awt.Component;
 import javax.swing.JToggleButton;
 
 public class InstructionsWindow extends javax.swing.JFrame {
     //initialize variables
+    private final Color PURPLE = new Color(58, 0, 82);
     private final String InstructionsText = "<html>Begin by choosing numbers for width by height, then bombs, then lives. 25x25 42-60 bombs, 1-3 lives is a good starting point, but choose whatever you want. Keep in mind it takes a little bit to pull up the window if you choose something over 200x200. (CTRL+Scroll to zoom... fair warning, 200x200 is 40,000 cells so its gonna take you a while.)<br><br>"
     +"If you click a bomb, you lose a life, lose all your lives to lose the game. Click a cell to reveal if it is a bomb. If your cell is not a bomb, it will instead display how many bombs are in the 8 surrounding cells, and if there are none, it will fill out all bombless cells surrounding the cell you clicked until no cell marked with a zero is adjacent to another cell that could be marked with a zero.<br>"
     +"The goal is to reveal every cell that is not a bomb. You may mark cells that contain bombs by use of the mark bombs button, or by right-clicking a cell. You cannot interact with marked cells other than to unmark them so as to prevent accidental death. If you mark twice it will instead receive a question mark, which is similar but interacts differently with chords.<br><br>"
     +"There is another method of interaction called a chord. If you use both mouse buttons when you click an already revealed numbered cell it clicks all surrounding squares but not an X mark, and if you marked too many it penalizes you for each extra mark by exploding a bomb each. However, this means it will reveal many more squares from just 1 action so it can be advantageous. It will explode question marks but not reveal if not a bomb.<br><br>"
     +"I have made a modification from the original ruleset in that you will always reveal a square with no adjacent bombs on your first click of the game instead of just requiring the cell itself to not be a bomb (unless your board size and #ofBombs prohibit this, in which case it will revert to the original ruleset).<br><br>"
     +"CTRL+SHIFT+Click on a score to delete it. Personal leaderboards will be saved in %userprofile%\\AppData\\Roaming\\minesweeperScores on windows, and ~/.minesweeper on linux or mac. If you alter the leaderboard manually and do not follow the format, scores will be unreadable by the program. format: x:y:bombCount:lives-livesLeft-time Additionally, if Leaderboard.txt is deleted or moved your scores can't be read until you move it back, but a new Leaderboard.txt file will be created to record future games.</html>";
-
+    private static final Icon DefaultButtonIcon = (new JButton()).getIcon();
+    private JButton Back = new JButton("Back");
+    private JToggleButton DMToggleButton = new JToggleButton("<html>Dark<br>Mode</html>");
+    private JLabel TitleLabel = new JLabel();
+    private JLabel instructions = new JLabel();
     void toggleDarkMode(){
-
+        if(MineSweeper.isDarkMode()){
+            Back.setBackground(Color.BLACK);
+            Back.setForeground(Color.WHITE);
+            DMToggleButton.setBackground(Color.BLACK);
+            DMToggleButton.setForeground(Color.WHITE);
+            instructions.setOpaque(true);
+            TitleLabel.setOpaque(true);
+            TitleLabel.setForeground(Color.GREEN);
+            instructions.setForeground(Color.WHITE);
+        }else{
+            Back.setBackground(null);
+            Back.setIcon(DefaultButtonIcon);
+            Back.setForeground(Color.BLACK);
+            DMToggleButton.setBackground(null);
+            DMToggleButton.setIcon(DefaultButtonIcon);
+            DMToggleButton.setForeground(Color.BLACK);
+            instructions.setOpaque(false);
+            TitleLabel.setOpaque(false);
+            TitleLabel.setForeground(PURPLE);
+            instructions.setForeground(Color.BLACK);
+        }
     }
     //constructors
     public InstructionsWindow() {
@@ -36,9 +65,14 @@ public class InstructionsWindow extends javax.swing.JFrame {
     }
     //init components
     private void initComponents() {
+        MetalToggleButtonUI toggleButtonSelectedColor = new MetalToggleButtonUI() {
+            @Override
+            protected Color getSelectColor() {
+                return (MineSweeper.isDarkMode())?PURPLE:super.getSelectColor();
+            }
+        };
+        DMToggleButton.setUI(toggleButtonSelectedColor);
         //listeners
-        JButton Back = new JButton("Back");
-        JToggleButton DMToggleButton = new JToggleButton("<html>Dark<br>Mode</html>");
         if(MineSweeper.isDarkMode())DMToggleButton.doClick();//<--sync toggle button status with dark mode status
         Back.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e) {
@@ -67,15 +101,37 @@ public class InstructionsWindow extends javax.swing.JFrame {
         Back.addKeyListener(keyAdapter);
         DMToggleButton.addKeyListener(keyAdapter);
 
-        JLabel TitleLabel = new JLabel();
         TitleLabel.setFont(new Font("Tahoma", 0, 36));
         TitleLabel.setText("Instructions");
         TitleLabel.setHorizontalAlignment(SwingConstants.CENTER);
 
-        JLabel instructions = new JLabel();
         instructions.setVerticalAlignment(SwingConstants.NORTH);
         instructions.setBorder(new EmptyBorder(10, 10, 10, 10));
         instructions.setText(InstructionsText);
+
+        instructions.setBackground(Color.BLACK);
+        TitleLabel.setBackground(Color.BLACK);
+        if(MineSweeper.isDarkMode()){
+            Back.setBackground(Color.BLACK);
+            Back.setForeground(Color.WHITE);
+            DMToggleButton.setBackground(Color.BLACK);
+            DMToggleButton.setForeground(Color.WHITE);
+            instructions.setOpaque(true);
+            TitleLabel.setOpaque(true);
+            TitleLabel.setForeground(Color.GREEN);
+            instructions.setForeground(Color.WHITE);
+        }else{
+            Back.setBackground(null);
+            Back.setIcon(DefaultButtonIcon);
+            Back.setForeground(Color.BLACK);
+            DMToggleButton.setBackground(null);
+            DMToggleButton.setIcon(DefaultButtonIcon);
+            DMToggleButton.setForeground(Color.BLACK);
+            instructions.setOpaque(false);
+            TitleLabel.setOpaque(false);
+            TitleLabel.setForeground(PURPLE);
+            instructions.setForeground(Color.BLACK);
+        }
 
         setIconImage(MineSweeper.MineIcon);
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
