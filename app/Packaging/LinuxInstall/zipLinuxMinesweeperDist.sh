8cd44e8ab6ec0c318ac57b8bd3dd4166df56cf88
BirdeeHub <robindbor@gmail.com>
2023-07-18 08:49:53 -0700

    changed names of installers

--- app/Packaging/LinuxInstall/zipLinuxMinesweeperDist.sh
+++ app/Packaging/LinuxInstall/zipLinuxMinesweeperDist.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-zip ../../../minesweeper_linux_dist.zip  ../../../minesweeper_4.0-1_amd64.deb uninstallLinuxMinesweeper.sh installLinuxMinesweeper.sh
+zip ../../../minesweeper_linux_dist.zip  ../../../minesweeper-4.0_1.0-1_amd64.deb uninstallLinuxMinesweeper.sh installLinuxMinesweeper.sh
