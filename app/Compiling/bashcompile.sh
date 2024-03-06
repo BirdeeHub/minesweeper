d684234d36bd35733f7f76a1e5ff9bd8aa9afefd
rdeboer-pzOtJY <robindbor@gmail.com>
2024-03-05 21:40:38 -0800

    in gradle this time

--- app/Compiling/bashcompile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-##these will be packaged with the application. If you edit the files in src, and you use this script, it will update the installed game.
-##if you mess up your game too much from editing, simply uninstall and reinstall from the installer. Your scores are saved elsewhere.
-javac ../src/MySweep/*.java ../src/*.java -d ../minesweeper_jar_in && cd ../minesweeper_jar_in/ && \
-jar --create -v --file=../minesweeper.jar --main-class=MySweep.MineSweeper  . ../src ../Compiling ../Packaging ../README.md  && \
-cd .. && rm -r ./minesweeper_jar_in/ && rm ./*.class
