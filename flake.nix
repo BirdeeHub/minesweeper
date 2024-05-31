bfd210c072888d6b8429f162382dc76119e872cd
BirdeeHub <robindbor@gmail.com>
2024-05-31 04:23:27 -0700

    gradle2nix test

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  inputs = {
+    gradle2nix.url = "github:tadfisher/gradle2nix/v2";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, ... }@inputs: let
+      forEachSystem = inputs.flake-utils.lib.eachSystem inputs.flake-utils.lib.allSystems;
+  in forEachSystem (system: let
+    pkgs = import inputs.nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+    defaultPackage = inputs.gradle2nix.builders.${system}.buildGradlePackage {
+      pname = "myProgram";
+      version = "1.0";
+      src = ./.;
+      lockFile = ./gradle.lock;
+      gradleFlags = [ "installDist" ];
+      installPhase = ''
+        mkdir -p $out/bin
+        mkdir -p $out/lib
+        cp ./app/build/install/app/bin/app $out/bin/minesweeper
+        cp ./app/build/install/app/lib/* $out/lib/
+      '';
+    };
+    jdk = pkgs.jdk;
+    gradle = pkgs.gradle;
+  in {
+    packages.default = defaultPackage;
+    devShells = {
+      default = pkgs.mkShell {
+        name = "gradle2nix";
+        packages = [ inputs.gradle2nix.packages.${system}.gradle2nix ];
+        inputsFrom = [ jdk gradle ];
+        GRADLE_HOME = "${gradle}";
+        JAVA_HOME = "${jdk}";
+        DEVSHELL = 0;
+        shellHook = ''
+          exec ${pkgs.zsh}/bin/zsh
+        '';
+      };
+    };
+  });
+}
